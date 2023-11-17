ce77fe825b0756080d831bc7378343d8aabd2a8b
Gabriel Sousa <gssousav@gmail.com>
2023-11-17 17:38:31 +0100

    Initial commit with guessing game code

--- guessing-game.js
+++ guessing-game.js
@@ -5,58 +5,60 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// const randomInRange = (min, max) => {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// const secretNumber = randomInRange(1, 100);
-
-// const checkGuess = (number) => {
-//     if (number > secretNumber) {
-//         console.log("Too high!");
-//         return false;
-//     } else if (number < secretNumber) {
-//         console.log("Too low!");
-//         return false;
-//     } else {
-//         console.log("You win!");
-//         return true;
-//     }   
-// }
-
-
-// function askGuess () {
-//     rl.question("Enter a guess: ", (guess) => {
-//         const userGuess = Number(guess);
-
-//         if (isNaN(userGuess)) {
-//             console.log("please enter a valid number");
-//             askGuess();
-//         }
-
-//         const isCorrect = checkGuess(userGuess);
-
-//         if (!isCorrect) {
-//             askGuess();
-//         } else {
-//             rl.close();
-//         }
-//     });
-// }
-
-// askGuess();
-
-function askRange () {
-    rl.question("Enter a minimum number: ", minNum);
-
-    function minNum (min) {
-        const lowerBound = Number(min);
-        rl.question("Enter a maximum number: ", maxNum);
-
-        function maxNum(max) {
-            const upperBound = Number(max);
-            rl.close();
-            console.log(`I'm thinking of a number between ${lowerBound} and ${upperBound}`)
-        }
-    }
+const checkGuess = (number, secretNumber) => {
+    if (number > secretNumber) {
+        console.log("Too high!");
+        return false;
+    } else if (number < secretNumber) {
+        console.log("Too low!");
+        return false;
+    } else {
+        console.log("You win!");
+        return true;
+    }   
 }
+
+
+function askGuess () {
+    rl.question("Enter a guess: ", (guess) => {
+        const userGuess = Number(guess);
+
+        if (isNaN(userGuess)) {
+            console.log("please enter a valid number");
+            askGuess();
+        }
+
+        const isCorrect = checkGuess(userGuess);
+
+        if (!isCorrect) {
+            askGuess();
+        } else {
+            rl.close();
+        }
+    });
+}
+
+const randomInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function askRange() {
+    rl.question("Enter a minimum number: ", (min) => {
+        const lowerBound = Number(min);
+
+        rl.question("Enter a maximum number: ", (max) => {
+            const upperBound = Number(max);
+
+            if (isNaN(lowerBound) || isNaN(upperBound) || lowerBound >= upperBound) {
+                console.log("Please enter valid minimum and maximum numbers. The maximum must be greater than the minimum.");
+                askRange();
+            } else {
+                console.log(`I'm thinking of a number between ${lowerBound} and ${upperBound}`);
+                const secretNumber = randomInRange(lowerBound, upperBound);
+                askGuess(secretNumber);
+            }
+        });
+    });
+}
+
+askRange();
