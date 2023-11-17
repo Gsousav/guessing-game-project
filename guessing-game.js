f1c9004ed75b17875e96b4a3bbf591ae7964b1a9
Gabriel Sousa <gssousav@gmail.com>
2023-11-17 17:30:17 +0100

    implement the user interface to input the range from which the program will guess

--- guessing-game.js
+++ guessing-game.js
@@ -5,39 +5,58 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const secretNumber = 5;
+// const randomInRange = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-const checkGuess = (number) => {
-    if (number > secretNumber) {
-        console.log("Too high!");
-        return false;
-    } else if (number < secretNumber) {
-        console.log("Too low!");
-        return false;
-    } else {
-        console.log("You win!");
-        return true;
-    }   
-}
+// const secretNumber = randomInRange(1, 100);
+
+// const checkGuess = (number) => {
+//     if (number > secretNumber) {
+//         console.log("Too high!");
+//         return false;
+//     } else if (number < secretNumber) {
+//         console.log("Too low!");
+//         return false;
+//     } else {
+//         console.log("You win!");
+//         return true;
+//     }   
+// }
 
 
-function askGuess () {
-    rl.question("Enter a guess: ", (guess) => {
-        const userGuess = Number(guess);
+// function askGuess () {
+//     rl.question("Enter a guess: ", (guess) => {
+//         const userGuess = Number(guess);
 
-        if (isNaN(userGuess)) {
-            console.log("please enter a valid number");
-            askGuess();
-        }
+//         if (isNaN(userGuess)) {
+//             console.log("please enter a valid number");
+//             askGuess();
+//         }
 
-        const isCorrect = checkGuess(userGuess);
+//         const isCorrect = checkGuess(userGuess);
 
-        if (!isCorrect) {
-            askGuess();
-        } else {
+//         if (!isCorrect) {
+//             askGuess();
+//         } else {
+//             rl.close();
+//         }
+//     });
+// }
+
+// askGuess();
+
+function askRange () {
+    rl.question("Enter a minimum number: ", minNum);
+
+    function minNum (min) {
+        const lowerBound = Number(min);
+        rl.question("Enter a maximum number: ", maxNum);
+
+        function maxNum(max) {
+            const upperBound = Number(max);
             rl.close();
+            console.log(`I'm thinking of a number between ${lowerBound} and ${upperBound}`)
         }
-    });
+    }
 }
-
-askGuess();
